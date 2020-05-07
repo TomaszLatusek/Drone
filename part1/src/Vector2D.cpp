b89a15626648ee2d63419edbd53b0867f49cfd0e
Tomasz Latusek <tomaszlatusek.tl@gmail.com>
2020-05-07 11:32:02 +0200

    add initial files

---
+++ part1/src/Vector2D.cpp
@@ -0,0 +1,9 @@
+#include "Vector2D.hh"
+
+
+/*
+ *  Tutaj nalezy zdefiniowac odpowiednie metody
+ *  klasy Vector2D, ktore zawieraja wiecej kodu
+ *  niz dwie linijki.
+ *  Mniejsze metody mozna definiwac w ciele klasy.
+ */
